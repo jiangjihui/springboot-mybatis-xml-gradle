6ab6014b5c0a3674e50cff23c6f768e7bb659afe
xqlsr <xqlsrjjjh@outlook.com>
2021-01-07 16:48:42 +0800

    init

--- src/test/java/com/example/demo/ApplicationTests.java
+++ src/test/java/com/example/demo/ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.jjh;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringbootMybatisXmlGradleApplicationTests {
+class ApplicationTests {
 
     @Test
     void contextLoads() {
